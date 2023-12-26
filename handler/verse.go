8824deb9258b37d3c8de16cd8d5218a1615be76b
Pedro Gardim <me@pedrogardim.com>
2023-12-26 17:43:14 +0100

    refactor: abstract GetVerseRangeByRef logic to repository

--- handler/verse.go
+++ handler/verse.go
@@ -1,12 +1,9 @@
 package handler
 
 import (
-	"fmt"
-	"scriptura/scriptura-api/db"
 	m "scriptura/scriptura-api/models"
 	r "scriptura/scriptura-api/repository"
 	"scriptura/scriptura-api/utils"
-	"strconv"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -14,57 +11,21 @@ import (
 func GetSingleVerse(c *fiber.Ctx) error {
 	bk, ch, vr := c.Params("book"), c.Params("chapter"), c.Params("verse")
 
-	verse, _ := r.GetVerseByRef("bible_en_kjv", bk, ch, vr)
+	verse, _ := r.GetVerseByRef("en_kjv", bk, ch, vr)
 
 	res := utils.FormatResponse(verse)
 	return c.JSON(res)
 }
 
 func GetVerseRange(c *fiber.Ctx) error {
-	db := db.DB
 	var verses []m.Verse
-	var totalItems int64
 
 	offset, limit := c.Locals("offset").(int), c.Locals("limit").(int)
-	book, chapter, verse := c.Params("book"), c.Params("chapter"), c.Params("verse")
+	bk, ch, fr, to := c.Params("book"), c.Params("chapter"), c.Params("from"), c.Params("to")
 
-	if chapter != "" || (chapter == "" && verse != "") {
-		if _, err := strconv.Atoi(chapter); err != nil {
-			return c.Status(fiber.ErrBadRequest.Code).JSON("Chapter number is no valid")
-		}
-	}
-
-	if verse != "" {
-		if _, err := strconv.Atoi(verse); err != nil {
-			return c.Status(fiber.ErrBadRequest.Code).JSON("Verse number is no valid")
-		}
-	}
-
-	query := db.Table("verse").
-		Select("verse.*, bible.text, book.name as book_name, COUNT(*) OVER () as totalItems").
-		Joins("left join book on book.id = book_id").
-		Joins("left join bible_en_kjv bible on bible.verse_id = verse.id").
-		Where("book.id::varchar ilike $1 OR book.code ilike $1 OR book.short_name ilike $1", book)
-
-	if len(chapter) > 0 || (len(chapter) == 0 && len(verse) > 0) {
-		query = query.Where("verse.chapter_num = ?", chapter)
-	}
-
-	if len(verse) > 0 {
-		query = query.Where("verse.verse_num = ?", verse)
-	}
-
-	query.Count(&totalItems)
-
-	query.Offset(offset).
-		Limit(limit).
-		Scan(&verses)
+	verses, totalItems, _ := r.GetVerseRangeByRef("en_kjv", bk, ch, fr, to, offset, limit)
 
 	response, _ := utils.FormatPaginationResponse(verses, int(totalItems), offset, limit)
 
 	return c.JSON(response)
-}
-
-func GetVerseRange(c *fiber.Ctx) error {
-	return c.JSON(c.Params("from") + " " + c.Params("to"))
 }
--- repository/verse.go
+++ repository/verse.go
@@ -1,6 +1,7 @@
 package repository
 
 import (
+	"fmt"
 	"scriptura/scriptura-api/db"
 	m "scriptura/scriptura-api/models"
 )
@@ -12,8 +13,8 @@ func GetVerseByRef(bible string, book string, chapter string, verse string) (m.V
 	db.Table("verse").
 		Select("verse.*, bible.text, book.name as book_name").
 		Joins("left join book on book.id = book_id").
-		Joins("left join bible_en_kjv bible on bible.verse_id = verse.id").
-		Where("book.id::varchar ilike $1 OR book.code ilike $1 OR book.short_name ilike $1", book).
+		Joins(fmt.Sprintf("left join bible_%s bible on bible.verse_id = verse.id", bible)).
+		Where("book.id::varchar ilike ? OR book.code ilike ? OR book.short_name ilike ?", book, book, book).
 		Where("verse.chapter_num = ?", chapter).
 		Where("verse.verse_num = ?", verse).
 		Scan(&verseResponse)
@@ -21,8 +22,23 @@ func GetVerseByRef(bible string, book string, chapter string, verse string) (m.V
 	return verseResponse, nil
 }
 
-// func getVerseRangeByRef(bible string, book string, chapter string, verse string) (m.Verse, error) {
-// 	db := db.DB
-// 	var verseResponse m.Verse
+func GetVerseRangeByRef(bible string, book string, chapter string, from string, to string, offset int, limit int) ([]m.Verse, int, error) {
+	db := db.DB
+	var versesResponse []m.Verse
+	var totalItems int64
 
-// }
+	query := db.Table("verse").
+		Select("verse.*, bible.text, book.name as book_name").
+		Joins("left join book on book.id = book_id").
+		Joins(fmt.Sprintf("left join bible_%s bible on bible.verse_id = verse.id", bible)).
+		Where("book.id::varchar ilike ? OR book.code ilike ? OR book.short_name ilike ?", book, book, book).
+		Where("verse.chapter_num = ?", chapter).
+		Where("verse.verse_num >= ? AND verse.verse_num <= ?", from, to)
+
+	query.Count(&totalItems)
+
+	query.Offset(offset).
+		Limit(limit).Scan(&versesResponse)
+
+	return versesResponse, int(totalItems), nil
+}
