c7890dcf9b95ed35fc9da2a7ca13347b0f1e01ef
Pedro Gardim <me@pedrogardim.com>
2024-02-21 18:59:41 +0100

    delete unused files

--- middleware/json.go
+++
@@ -1,6 +0,0 @@
-package middleware
-
-// func JsonMiddleware(c *fiber.Ctx) error {
-// 	c.Set(fiber.HeaderContentType, fiber.MIMEApplicationJSONCharsetUTF8)
-// 	return c.Next()
-// }
--- router/router.go
+++ router/router.go
@@ -25,30 +25,3 @@ func NewAppRouter(repo *repository.AppRepository, handlers *handler.AppHandlers)
 
 	return mainMux
 }
-
-//OLD------------V
-/*
-func SetupRoutes(app fiber.Router) {
-	api := app.Group("/api/v1", logger.New())
-
-	app.Use(swagger.New(swagger.Config{
-		FilePath: "docs/swagger.json",
-		Path:     "swagger",
-	}))
-
-	api.Use(m.JsonMiddleware)
-
-	api.Get("/book/:book", handler.GetBook)
-	api.Get("/chapter/:chapter", handler.GetChapter)
-
-	bible := api.Group("/bible", m.PaginationMiddleware)
-
-	bible.Get("/book/:book", handler.GetBible)
-
-	bible.Get("/book/:book/chapter/:chapter", handler.GetBible)
-	bible.Get("/book/:book/chapter/:chapter/verse/:start-:end", handler.GetBible)
-	bible.Get("/book/:book/chapter/:chapter/verse/:start", handler.GetBible)
-
-	app.Post("/graphql", gql.Handler)
-}
-*/
